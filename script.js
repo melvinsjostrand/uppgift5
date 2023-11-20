ee6449a9d9dc749671d2f94499da86aa779e913f
melvinsjostrand <mesj04001@utb.vaxjo.se>
2023-11-20 14:23:55 +0100

    uppgift 5

--- script.js
+++ script.js
@@ -1,89 +1,101 @@
 // JavaScript
 
 // Globala variabler
-let inputElem;
-let msgElem;
+let msgElem; //
+let inputElem; //
 let fruitNames;
-let fruitnr; 
+let fruitNr = 0;
+let nr = 0;
 let selFruitsElem;
+let errCount = 0;
 
 // Funktion som körs då hela webbsidan är inladdad, dvs då all HTML-kod är utförd.
 // Initiering av globala variabler samt koppling avfunktioner till knapparna.
-function init() 
-{
-        fruitnr = 0;
-        fruitNames = ["ingen bild","äpple","banan","citron","apelsin",+"päron"];
-	    inputElem=[];
-        inputElem[1]=document.getElementById("input1");
-        inputElem[2]=document.getElementById("input2");
-        inputElem[3]=document.getElementById("input3");
-        selFruitsElem = document.getElementById("selectedFruits");
-        msgElem=document.getElementById("message");
-            document.getElementById("btn1").onclick=showFruit;
-            document.getElementById("btn2").onclick=checkName;
-            document.getElementById("btn3").onclick=addFruits;
+function init() {
+    inputElem = [];
+    inputElem[1] = document.getElementById("input1");
+    inputElem[2] = document.getElementById("input2");
+    inputElem[3] = document.getElementById("input3");
+    msgElem = document.getElementById("message");
+    document.getElementById("btn1").onclick=showFruit;
+    document.getElementById("btn2").onclick=checkName;
+    document.getElementById("btn3").onclick=addFruits;
+    fruitNames = ["ingen frukt", "äpple", "banan", "citron", "appelsin", "päron"];
+    selFruitsElem = document.getElementById("selectedFruits");
 } // End init
 window.onload = init; // Se till att init aktiveras då sidan är inladdad
 
-
-function showFruit() //visar bild på olika frukter beroende på vilket nummer du väljer.
-{
+//Tar input från html textfält och först kollar att det är en siffra, därefter ifall det är inom intervallet för att sedan visa bild med url kopplad till siffra i textfält
+function showFruit(){
     let nr;
-    let fruitURL;
+    nr = getNr(1,5);
+    if(nr != null){
 
-    nr = getnr(1,5); 
-    
-    if(getnr !== null){
-        fruitURL = "./pics/fruit" + nr +".jpg";
-        document.getElementById("fruitImg").src =  fruitURL;
-    }else {
-        msgElem.innerHTML = "<p> dåligt</p>"
+        let fruitUrl = "./pics/fruit" + nr + ".jpg"; //Om talet är inannför intervallet visas en bild även om det finns decimaltal, då parseInt tar bort eventuella decimaler
+        document.getElementById("fruitImg").src = fruitUrl; //Bild ändras med samma url som fruitUrl
+        msgElem.innerHTML = "";
+        errCount = 0;
     }
+    else{
+        errCount++;
+        msgElem.innerHTML = "<p>Inte en giltig siffra i inmatningsfält! "+errCount+"</p>";
 
-    fruitnr = nr;
+    }
+    fruitNr = nr;
 }
-
-
 function checkName(){
-    let name;
-    name = (inputElem[2].value);
-
-    if(fruitnr === 0 ){
-        msgElem.innerHTML = "<p>välj en frukt tack</p>";
+    let name = inputElem[2].value;
+    console.log(fruitNr);
+    if(fruitNr === 0){
+        msgElem.innerHTML = "<p>Välj en frukt först</p>";
         return;
     }
 
-    if(name === fruitNames[fruitnr]) {
+    if(name === fruitNames[fruitNr]){
         msgElem.innerHTML = "<p>Rätt namn</p>";
-    } else {
-        msgElem.innerHTML = "<p>fel namn</p>";
+    }
+    else{
+        msgElem.innerHTML = "<p>Fel namn</p>";
     }
 }
-function getnr(elemnr, high){
-    
+
+function getNr(elemNr, high){
     let nr;
-    nr = (inputElem[elemnr].value);
+    //let elemNr = 1;
+
+    //let high = 5;
+
+    nr = Number(inputElem[elemNr].value); 
     if(isNaN(nr)){
-        msgElem.innerHTML = "<p> skriv ett nummer mellan 1-5</p>"
         return null;
     }
-
-    if(nr>=1&&nr<=high){
-        nr = parseInt(nr);
-        inputElem[elemnr].value = nr;    
-
-    }else{
-        msgElem.innerHTML = "<p> skriv ett nummer mellan 1-5</p>"
+    if(nr < 1 || nr > high){ 
+        msgElem.innerHTML = "<p>Skriv en siffra mellan 1 och 5</p>"; //Om talet är utanför intervallet skrivs en kommentar som förklarar varför ingen bild visas
         return null;
+
     }
+    nr = parseInt(nr);
+    inputElem[elemNr].value = nr;
     return nr;
 }
 
 function addFruits(){
-    let amount;
-    let imgList;
-    amount = inputElem[3].value;
-    imgList =  selFruitsElem.src =  fruitURL;
+    //let amount = inputElem[3].value;
+    //console.log(amount + " " + fruitNr);
 
-    if()
+    if(fruitNr==0){
+        return;
+    }
+
+    amount = getNr(3,9);
+    console.log(amount + " " + fruitNr);
+
+    let imgList = "";
+    if(amount!=null){
+        for(i = 0; i < amount; i++){
+            imgList+="<img src='./pics/fruit" + fruitNr + ".jpg' alt='frukt'>";
+        }
+    }
+    console.log(imgList);
+    selFruitsElem.innerHTML += imgList;
 }
